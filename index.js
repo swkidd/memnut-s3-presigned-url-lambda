94132b1a1f3df2ab7d24bafd14e0bac59a01f9d1
swkidd <kiddsoftware@gmail.com>
2021-07-07 23:40:16 +0900

    memages

--- index.js
+++ index.js
@@ -1,50 +1,65 @@
-const AWS = require('aws-sdk');
-const { "v4": uuidv4 } = require('uuid');
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 
-const BUCKET_NAME = process.env['BUCKET_NAME'];
+const BUCKET_NAME = process.env["BUCKET_NAME"];
 
 const s3 = new AWS.S3();
 
-exports.handler = async event => {
-  const requestContext = event.requestContext
+exports.handler = async (event) => {
+  const requestContext = event.requestContext;
   try {
-    const body = JSON.parse(event.body)
-    const markerid = body.markerid || uuidv4()
-    const fileType = body.fileType
-    const latlng = body.latlng
-    const type = body.type
-    const claims = requestContext.authorizer.jwt.claims
-    const email = claims.email
-    const creator = (u => ({
+    const body = JSON.parse(event.body);
+    const type = body.type;
+    const fileType = body.fileType;
+    const claims = requestContext.authorizer.jwt.claims;
+    const email = claims.email;
+    const creator = ((u) => ({
       name: u.name,
       family_name: u.family_name,
       given_name: u.given_name,
       picture: u.picture,
-    }))(claims)
+    }))(claims);
 
-    if (!(fileType === 'image/jpeg' || fileType === 'image/png' || fileType === 'image/webp')) {
-      throw new Error("invalid request")
+    if (
+      !(
+        fileType === "image/jpeg" ||
+        fileType === "image/png" ||
+        fileType === "image/webp"
+      )
+    ) {
+      throw new Error("invalid request");
     }
 
-    if (!latlng || !creator.name || !email || !type ) {
-      throw new Error("invalid request")
+    if (!creator.name || !email || !type) {
+      throw new Error("invalid request");
     }
+
+    let fields = {
+      key: email,
+      "Content-Type": fileType,
+      "Cache-Control": "public",
+      "x-amz-meta-type": type,
+      "x-amz-meta-creator": JSON.stringify(creator),
+    };
+
+    if (type === "marker") {
+      const markerid = body.markerid || uuidv4();
+      const latlng = body.latlng;
+      fields["x-amz-meta-markerid"] = markerid;
+      fields["x-amz-meta-latlng"] = JSON.stringify(latlng);
+    } else if (type === "mem") {
+      const memageid = body.memageid || uuidv4();
+      fields["x-amz-meta-memageid"] = memageid;
+    }
+
     let params = {
       Bucket: BUCKET_NAME,
-      Fields: {
-        key: email,
-        'Content-Type': fileType,
-        'Cache-Control': 'public',
-        'x-amz-meta-markerid': markerid,
-        'x-amz-meta-type': type,
-        'x-amz-meta-latlng': JSON.stringify(latlng),
-        'x-amz-meta-creator': JSON.stringify(creator)
-      },
+      Fields: fields,
       Expires: 300,
       Conditions: [
         ["content-length-range", 0, 20971520],
-        { 'Content-Type': fileType }
-      ]
+        { "Content-Type": fileType },
+      ],
     };
 
     let data = await createPresignedPostPromise(params);
@@ -53,12 +68,10 @@ exports.handler = async event => {
       statusCode: 200,
       body: JSON.stringify({ data }),
     };
-  }
-  catch (error) {
+  } catch (error) {
     return {
       statusCode: 400,
-    }
-
+    };
   }
 };
 
@@ -67,6 +80,6 @@ function createPresignedPostPromise(params) {
     s3.createPresignedPost(params, (err, data) => {
       if (err) reject(err);
       else resolve(data);
-    })
+    });
   });
 }
